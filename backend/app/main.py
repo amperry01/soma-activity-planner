6079ec5239d0db3b9d7ef84e0d7657d58104db6d
Alyssa Perry <alyperry01@gmail.com>
2025-08-15 22:56:14 -0700

    register new routes in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,2 +1,7 @@
 from fastapi import FastAPI
-from app.routes 
+from app.routes import recommendations, oura_loader
+
+app = FastAPI()
+
+app.include_router(recommendations.router)
+app.include_router(oura_loader.router)
